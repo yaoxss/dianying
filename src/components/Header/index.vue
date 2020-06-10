e93c2ee9d667cb106b3d5c71d742fce01374aa34
yaoxss <yxsaio2009@163.com>
2020-06-10 13:53:43 +0800

    路由的基本链接

---
+++ src/components/Header/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <header id="header">
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: {
+            title: {
+                type: String,
+                default: "喵喵电影"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /*position: relative; 相对定位  absolute绝对定位*/
+    #header{width:100%; height:50px; color: #fff; background: #e54847; border-bottom: 1px solid #e54847; position: relative;}
+    #header h1{ font-size: 18px; text-align: center; line-height: 50px; font-weight: normal; }
+    #header i{ position: absolute; left: 5px; top: 50%; margin-top: -13px; font-size: 26px;}
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Header title="个人信息"></Header>
+        <LoginYao></LoginYao>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header';
+    import TabBar from '@/components/TabBar';
+    import LoginYao from '@/components/Login';
+    export default {
+        name: "Mine",
+        components: {
+            Header,
+            TabBar,
+            LoginYao
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
